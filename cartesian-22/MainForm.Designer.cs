48534ba37ee4cdd50112d9b25b33874f897d3f4f
Tom Gregor <tgregor@ivsoftware.com>
2022-08-02 12:47:21 -0600

    Add project files.

---
+++ cartesian-22/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace cartesian_22
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.Location = new System.Drawing.Point(31, 22);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(900, 900);
+            this.panel.TabIndex = 0;
+            this.panel.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 944);
+            this.Controls.Add(this.panel);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+    }
+}
+
---
+++ cartesian-22/MainForm.cs
@@ -0,0 +1,106 @@
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace cartesian_22
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            _chart = new Chart
+            {
+                Dock = DockStyle.Fill,
+            };
+            _series = new Series
+            {
+                Name = "Series1",
+                Color = Color.Green,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = false,
+                ChartType = SeriesChartType.Line,
+            };
+            _series.Points.Add(new DataPoint { IsEmpty = true });
+
+            _chartArea = new ChartArea
+            {
+                AxisX =
+                {
+                    Minimum = -10,
+                    Maximum = 10,
+                    Interval = 1,
+                    IsMarksNextToAxis = false,
+                    IsLabelAutoFit = false,
+                    Crossing = 0,
+                    LineWidth = 1,
+                    MajorGrid = {  Interval = 1, LineColor = Color.LightBlue, LineWidth = 1 },
+                    Enabled = AxisEnabled.True,
+                },
+                AxisY =
+                {
+                    Minimum = -10,
+                    Maximum = 10,
+                    Interval = 1,
+                    IsMarksNextToAxis = false,
+                    IsLabelAutoFit = false,
+                    Crossing = 0,
+                    LineWidth = 1,
+                    MajorGrid = {  Interval = 1, LineColor = Color.LightBlue, LineWidth = 1 },
+                    Enabled = AxisEnabled.True,
+                },
+            };
+
+            _chart.ChartAreas.Add(_chartArea);
+            _series.MarkerStyle = MarkerStyle.Cross;
+            _chart.Series.Add(_series);
+
+            _chartArea.CursorX.LineColor = Color.LightCoral;
+            _chartArea.CursorX.Interval = 0.01;
+            _chartArea.CursorX.IsUserEnabled = true;
+
+            _chartArea.CursorY.LineColor = Color.LightCoral;
+            _chartArea.CursorY.Interval = 0.01;
+            _chartArea.CursorY.IsUserEnabled = true;
+
+            panel.Controls.Add(_chart);
+#if false
+            // Draw diagonal
+            for (int i = -10; i <= 10; i++)
+            {
+                _series.Points.AddXY(i, i);
+            }
+#endif
+            _chart.MouseClick += onChartClick;
+            _chart.MouseDoubleClick += onChartDoubleClick;
+            _chart.MouseMove += onChartMouseMove;
+        }
+        private void onChartMouseMove(object? sender, MouseEventArgs e)
+        {
+            _chartArea.CursorX.SetCursorPixelPosition(e.Location, true);
+            _chartArea.CursorY.SetCursorPixelPosition(e.Location, true);
+        }
+
+        private void onChartClick(object? sender, MouseEventArgs e)
+        {
+            _series.Points.AddXY(_chartArea.CursorX.Position, _chartArea.CursorY.Position);
+        }
+
+        private void onChartDoubleClick(object? sender, MouseEventArgs e)
+        {
+            _series.Points.Clear();
+            _series.Points.Add(new DataPoint { IsEmpty = true });
+        }
+
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            Task.Delay(100).GetAwaiter().OnCompleted(() =>
+            {
+                panel.Visible = true;
+            });
+        }
+        private readonly Series _series;
+        private readonly Chart _chart;
+        private readonly ChartArea _chartArea;
+    }
+}
